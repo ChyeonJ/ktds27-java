b1725da47c733252530fc810b60d4ecfb5c48eb8
ChyeonJ <akdntm4757@gmail.com>
2026-02-04 09:49:33 +0900

    중간저장

--- article/src/com/ktdsuniversity/edu/file/Recursive.java
+++ article/src/com/ktdsuniversity/edu/file/Recursive.java
@@ -1,6 +1,42 @@
 package com.ktdsuniversity.edu.file;
 
+import java.io.File;
+
 public class Recursive {	//재귀 호출
+	
+	public void deleteDirectory(File target) {
+		if(target.isFile()) {
+			System.out.println(target.getAbsolutePath());
+			target.delete();
+		} else if(target.isDirectory()) {
+			//폴더 내부의 목록을 조회한다.
+			File[] children = target.listFiles(); //파일을 배열로 받아온다
+			
+			for(File child : children) {
+				//System.out.println(child);	//.getAbsolutePath() / 폴더나 파일의 절대 경로를 알려준다
+				this.deleteDirectory(child);
+			}
+			
+			System.out.println(target.getAbsolutePath());
+			target.delete();
+		}
+	}
+	
+	public void printFiles(File target) {
+		
+		if(target.isFile()) {
+			System.out.println(target.getAbsolutePath());
+		} else if(target.isDirectory()) {
+			//폴더 내부의 목록을 조회한다.
+			File[] children = target.listFiles(); //파일을 배열로 받아온다
+			
+			for(File child : children) {
+				//System.out.println(child);	//.getAbsolutePath() / 폴더나 파일의 절대 경로를 알려준다
+				this.printFiles(child);
+			}
+		}
+		
+	}
 	
 	public void printNumber(int number) {
 		System.out.println( (number) + " 스택 실행을 완료했습니다.");
@@ -30,9 +66,17 @@ public class Recursive {	//재귀 호출
 	public static void main(String[] args) {
 		Recursive r = new Recursive();
 		//r.print(1);
-		r.printNumber(2000); // 결과 => 2000 ~ 0
+		//r.printNumber(2000); // 결과 => 2000 ~ 0
 		//int result = r.sumToZero(5);
 		//System.out.println(result);
+		
+		//금일 수업에서는  user_home경로가 아닌 C밑에 우리의 워크스페이스에서 진행할 거여서 아래처럼 작성
+//		File root = new File("C:/DevPrograms");
+//		r.printFiles(root);
+		
+		File root = new File("C:\\Devprograms\\삭제대상");
+		r.deleteDirectory(root);
+		
 	}
 
 }
