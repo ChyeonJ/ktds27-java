aa7cd8dd20fec8aa9aa056c792136f2e6f95c063
ChyeonJ <akdntm4757@gmail.com>
2026-05-01 16:48:15 +0900

    article

--- react-apps/hello-react-2/src/stores/toolkit/slices/articleSlice.js
+++ react-apps/hello-react-2/src/stores/toolkit/slices/articleSlice.js
@@ -1,3 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 // ReduxToolkit slice store 생성
 // export const articleSlice = createSlice({});
+
+export const articleSlice = createSlice({
+  name: "article-slice",
+  initialState: {
+    list: [],
+  },
+  reducers: {
+    laodArticleList(store, action) {
+      store.list = action.payload;
+    },
+    jsonWebToken(store, action) {
+      store.list = action.payload;
+    },
+    addArticle(store, action) {
+      store.list = action.payload;
+    },
+  },
+});
